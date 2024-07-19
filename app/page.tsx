278acbed9e88e9516da706227f0b7effab2d9627
Bruno Grecco <bruno.grecco.a@gmail.com>
2024-07-19 18:23:21 -0300

    Added skills section to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "./_sections/About";
+import Skills from "./_sections/Skills";
 import Contact from "./_sections/Contact";
 import Projects from "./_sections/Projects";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <>
       <About />
+      <Skills />
       <Projects />
       <Contact />
     </>
