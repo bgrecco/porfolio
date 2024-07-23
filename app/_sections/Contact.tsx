d160179ff2a4e820865bb3f4afc92a894ebb31da
Bruno Grecco <bruno.grecco.a@gmail.com>
2024-07-22 21:01:44 -0300

    Updated placeholder contact section

--- app/_sections/Contact.tsx
+++ app/_sections/Contact.tsx
@@ -1,3 +1,5 @@
+import ContactForm from "../_components/ContactForm";
+import HighlightedText from "../_components/HighlightedText";
 import Section from "../_components/Section";
 import { BriefcaseIcon } from "@heroicons/react/24/outline";
 
@@ -20,18 +22,16 @@ export default function Contact() {
     >
       <div className="flex flex-col w-full space-y-24 mt-3 lg:text-left">
         <div className="flex flex-col text-center lg:text-left space-y-4">
-          <div className="relative mx-auto lg:mr-[611.67px]">
-            <div className="absolute -inset-0 text-5xl text-secondary-light font-extrabold blur-md opacity-75 lg:text-left lg:text-nowrap">
-              Lets work together
-            </div>
-            <h1 className="relative text-5xl text-secondary-light font-extrabold lg:text-left lg:text-nowrap">
-              Lets work together
-            </h1>
-          </div>
+          <HighlightedText
+            text={"Lets work together"}
+            fontSize={"text-5xl"}
+            marginRight={"lg:mr-[611.67px]"}
+          />
           <p className="max-w-screen-sm text-base lg:ml-0.5">
             Lets build your next idea
           </p>
         </div>
+        <ContactForm />
       </div>
     </Section>
   );
